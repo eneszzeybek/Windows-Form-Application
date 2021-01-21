2f3c23e030b24e780bb447fbfc37d4e95bb1f947
Enes Zeybek <63055251+eneszzeybek@users.noreply.github.com>
2021-01-22 01:40:02 +0300

    Add project files.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double number1, number2, add;
+            number1 = Convert.ToDouble(textBox1.Text);
+            number2 = Convert.ToDouble(textBox2.Text);
+            add = number1 + number2;
+            textBox3.Text = add.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double number1, number2, subtract;
+            number1 = Convert.ToDouble(textBox1.Text);
+            number2 = Convert.ToDouble(textBox2.Text);
+            subtract = number1 - number2;
+            textBox3.Text = subtract.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double number1, number2, multiply;
+            number1 = Convert.ToDouble(textBox1.Text);
+            number2 = Convert.ToDouble(textBox2.Text);
+            multiply = number1 * number2;
+            textBox3.Text = multiply.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double number1, number2, divide;
+            number1 = Convert.ToDouble(textBox1.Text);
+            number2 = Convert.ToDouble(textBox2.Text);
+            divide = number1 / number2;
+            textBox3.Text = divide.ToString();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',';
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',';
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',';
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
